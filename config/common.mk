a568fff76171b201750d826e6cccb22e3834fee9
Eliminater74 <eliminater74@gmail.com>
2017-08-25 11:56:18 +0530

    Allow insecure ADB logging at boot and removed default disabled properties
    Change-Id: I0c16b8a2a9e72cc3960d260221d5f6c8aff583d8

--- config/common.mk
+++ config/common.mk
@@ -67,10 +67,16 @@ ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 endif
 
-ifneq ($(TARGET_BUILD_VARIANT),eng)
+# ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
-endif
+# ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+# endif
+
+# Allow recording ADB logcat during boot phase
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    persist.service.adb.enable=1
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
